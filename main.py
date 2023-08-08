d3b0b6a8220f32c8d0c6e8062fd62dac17397fe8
Kevyn <kevynsilveira0610@gmail.com>
2023-08-08 09:06:23 -0300

    Feito a função executar

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
 import os
 from PyPDF2 import PdfMerger
 import customtkinter as ctk
+from tkinter import messagebox
+import datetime
 
-def get_creation_time(file_path):
-    # Obter o timestamp de criação do arquivo
+def get_current_date_formatted():
+    current_date = datetime.datetime.now()
+    return current_date.strftime("%d-%m-%Y")
+
+def get_creation_time(file_path):# Obter o timestamp de criação do arquivo
     return os.path.getctime(file_path)
-
 
 def merge_pdfs_in_folder(folder_path, output_file, order_by="date", reverse_order=False):
     merger = PdfMerger()
     pdf_files = [(get_creation_time(os.path.join(folder_path, f)), f) for f in os.listdir(folder_path) if f.endswith(".pdf")]
 
-    if order_by == "date":
+    if order_by == "Data":
         pdf_files.sort(key=lambda x: x[0], reverse=reverse_order)
-    elif order_by == "crescente":
+    elif order_by == "Crescente":
         pdf_files.sort(key=lambda x: x[1], reverse=reverse_order)
-    elif order_by == "decrescente":
+    elif order_by == "Decrescente":
         pdf_files.sort(key=lambda x: x[1], reverse=not reverse_order)
 
     for timestamp, filename in pdf_files:
@@ -25,7 +29,24 @@ def merge_pdfs_in_folder(folder_path, output_file, order_by="date", reverse_orde
     with open(output_file, "wb") as output_pdf:
         merger.write(output_pdf)
 
-    print("PDFs combinados com sucesso!")
+    messagebox.showinfo("ATENÇÃO","PDFs combinados com sucesso!")
+
+def execute():
+    diretory = entry_select.get()
+    order = combobox_order.get()
+    name_file = get_current_date_formatted()
+    reverse_option = False
+
+    if order == "Crescente":
+        reverse_option = True
+    if order == "Descrente":
+        reverse_option = False
+
+    if (diretory != "" and diretory != None and order != "" and order != None):
+        folder_path = "C:\\Users\\Kevyn\\Documents\\origem"
+        output_file = f"C:\\Users\\Kevyn\\Documents\\Destino\\{name_file}.pdf"
+        merge_pdfs_in_folder(folder_path, output_file, order, reverse_option)
+
 
 frame = ctk.CTk()
 frame.geometry("250x200")
@@ -47,11 +68,6 @@ combobox_order.place(x=50, y=90)
 button_execute = ctk.CTkButton(master=frame, text= "Executar", width=150, height=30)
 button_execute.place(x=50, y=160)
 
-
 if __name__ == "__main__":
-    # Substitua "caminho/da/pasta" pelo caminho real da pasta que contém os arquivos PDF
-    folder_path = "C:\\Users\\Kevyn\\Documents\\origem"
-    # Substitua "caminho/do/arquivo_completo.pdf" pelo caminho do arquivo que será gerado
-    output_file = "C:\\Users\\Kevyn\\Documents\\Destino\\arquivo_completo.pdf"
     frame.mainloop()
-    #merge_pdfs_in_folder(folder_path, output_file)
+
