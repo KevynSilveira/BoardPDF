4a12869872247d1c5159c665ab9d1f3c61e939d0
Kevyn <kevynsilveira0610@gmail.com>
2023-08-08 09:37:49 -0300

    Finalizado versão 1.0 que ordena por ordem crescente

--- main.py
+++ main.py
@@ -3,7 +3,14 @@ from PyPDF2 import PdfMerger
 import customtkinter as ctk
 from tkinter import messagebox
 import datetime
+from tkinter import filedialog
+import tkinter as tk
+from natsort import natsorted
 
+def select():
+    diretorio = filedialog.askdirectory()
+    entry_select.delete(0, tk.END)
+    entry_select.insert(0, diretorio)
 def get_current_date_formatted():
     current_date = datetime.datetime.now()
     return current_date.strftime("%d-%m-%Y")
@@ -11,61 +18,41 @@ def get_current_date_formatted():
 def get_creation_time(file_path):# Obter o timestamp de criação do arquivo
     return os.path.getctime(file_path)
 
-def merge_pdfs_in_folder(folder_path, output_file, order_by="date", reverse_order=False):
+def merge_pdfs_in_folder(folder_path, output_file):
     merger = PdfMerger()
-    pdf_files = [(get_creation_time(os.path.join(folder_path, f)), f) for f in os.listdir(folder_path) if f.endswith(".pdf")]
+    pdf_files = natsorted([f for f in os.listdir(folder_path) if f.endswith(".pdf")])
 
-    if order_by == "Data":
-        pdf_files.sort(key=lambda x: x[0], reverse=reverse_order)
-    elif order_by == "Crescente":
-        pdf_files.sort(key=lambda x: x[1], reverse=reverse_order)
-    elif order_by == "Decrescente":
-        pdf_files.sort(key=lambda x: x[1], reverse=not reverse_order)
-
-    for timestamp, filename in pdf_files:
+    for filename in pdf_files:
         pdf_path = os.path.join(folder_path, filename)
         merger.append(pdf_path)
 
     with open(output_file, "wb") as output_pdf:
         merger.write(output_pdf)
 
-    messagebox.showinfo("ATENÇÃO","PDFs combinados com sucesso!")
+    messagebox.showinfo("ATENÇÃO", "PDFs combinados com sucesso!")
+
 
 def execute():
-    diretory = entry_select.get()
-    order = combobox_order.get()
-    name_file = get_current_date_formatted()
-    reverse_option = False
+    directory = entry_select.get()
 
-    if order == "Crescente":
-        reverse_option = True
-    if order == "Descrente":
-        reverse_option = False
-
-    if (diretory != "" and diretory != None and order != "" and order != None):
-        folder_path = "C:\\Users\\Kevyn\\Documents\\origem"
-        output_file = f"C:\\Users\\Kevyn\\Documents\\Destino\\{name_file}.pdf"
-        merge_pdfs_in_folder(folder_path, output_file, order, reverse_option)
-
+    if directory:
+        folder_path = directory
+        output_file = filedialog.asksaveasfilename(defaultextension=".pdf", filetypes=[("PDF files", "*.pdf")], initialfile=f"{get_current_date_formatted()}.pdf")
+        if output_file:
+            merge_pdfs_in_folder(folder_path, output_file)
 
 frame = ctk.CTk()
 frame.geometry("250x200")
 frame.title("BoardPDF")
 frame.resizable(False, False)
 
-button_select = ctk.CTkButton(master=frame, text= "Diretório origem", width=150, height=30)
+button_select = ctk.CTkButton(master=frame, text= "Diretório origem", width=150, height=30, command=select)
 button_select.place(x=50, y=10)
 
 entry_select = ctk.CTkEntry(master=frame, width=200, height=30)
 entry_select.place(x=25, y=50)
 
-option_order = ["Data", "Crescente", "Decrescente"]  # Opções
-combobox_order = ctk.CTkComboBox(master=frame, values=option_order, width=150, height=30, state="readonly")
-combobox_order.set("Ordenação")  # Indica que precisa escolher um ordenação
-combobox_order.configure(justify="center")  # Centraliza o texto na combobox
-combobox_order.place(x=50, y=90)
-
-button_execute = ctk.CTkButton(master=frame, text= "Executar", width=150, height=30)
+button_execute = ctk.CTkButton(master=frame, text= "Executar", width=150, height=30, command=execute)
 button_execute.place(x=50, y=160)
 
 if __name__ == "__main__":
