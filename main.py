64f4e83f5e9d5e584ca027cf0f7c9c155f92fd65
Kevyn <kevynsilveira0610@gmail.com>
2023-08-09 09:11:23 -0300

    Feito alteração no front-end do código. Adicionado a label de BoardPDF. Feito a melhoria no designer dos Botões.

--- main.py
+++ main.py
@@ -1,24 +1,26 @@
 import os
-from PyPDF2 import PdfMerger
-import customtkinter as ctk
-from tkinter import messagebox
 import datetime
-from tkinter import filedialog
+import customtkinter as ctk
 import tkinter as tk
+from tkinter import messagebox
+from tkinter import filedialog
+from customtkinter import set_default_color_theme
 from natsort import natsorted
+from PyPDF2 import PdfMerger
 
-def select():
+def select(): # Seleciona o diretório do PDF
     diretorio = filedialog.askdirectory()
     entry_select.delete(0, tk.END)
     entry_select.insert(0, diretorio)
-def get_current_date_formatted():
+
+def get_current_date_formatted(): # Retorna a data formatada
     current_date = datetime.datetime.now()
     return current_date.strftime("%d-%m-%Y")
 
 def get_creation_time(file_path):# Obter o timestamp de criação do arquivo
     return os.path.getctime(file_path)
 
-def merge_pdfs_in_folder(folder_path, output_file):
+def merge_pdfs_in_folder(folder_path, output_file): # Junta os PDF em forma crescente
     merger = PdfMerger()
     pdf_files = natsorted([f for f in os.listdir(folder_path) if f.endswith(".pdf")])
 
@@ -31,8 +33,7 @@ def merge_pdfs_in_folder(folder_path, output_file):
 
     messagebox.showinfo("ATENÇÃO", "PDFs combinados com sucesso!")
 
-
-def execute():
+def execute(): # Executa as ações
     directory = entry_select.get()
 
     if directory:
@@ -45,14 +46,22 @@ frame = ctk.CTk()
 frame.geometry("250x200")
 frame.title("BoardPDF")
 frame.resizable(False, False)
+set_default_color_theme("dark-blue")
 
-button_select = ctk.CTkButton(master=frame, text= "Diretório origem", width=150, height=30, command=select)
-button_select.place(x=50, y=10)
+label_board_pdf = ctk.CTkLabel(master=frame, text="BoardPDF", width=150, height=30, text_color="white")
+label_board_pdf.configure(font=("arial", 18))
+label_board_pdf.place(x=50, y=5)
+
+button_select = ctk.CTkButton(master=frame, text= "Diretório origem", width=150, height=30, command=select, fg_color="dark grey", text_color="black")
+button_select.configure(font=("arial", 14))
+button_select.place(x=50, y=50)
 
 entry_select = ctk.CTkEntry(master=frame, width=200, height=30)
-entry_select.place(x=25, y=50)
+entry_select.configure(font=("arial", 14))
+entry_select.place(x=25, y=90)
 
-button_execute = ctk.CTkButton(master=frame, text= "Executar", width=150, height=30, command=execute)
+button_execute = ctk.CTkButton(master=frame, text= "Executar", width=150, height=30, command=execute, fg_color="gray", text_color="black")
+button_execute.configure(font=("arial", 14))
 button_execute.place(x=50, y=160)
 
 if __name__ == "__main__":
