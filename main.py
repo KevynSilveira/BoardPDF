b5a9f5795e891fac15261173c2015e732bfc73ec
Kevyn <kevynsilveira0610@gmail.com>
2023-08-09 09:32:48 -0300

    Feito alteração no front da versão 1.1

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import customtkinter as ctk
 import tkinter as tk
 from tkinter import messagebox
 from tkinter import filedialog
-from customtkinter import set_default_color_theme
+from customtkinter import set_appearance_mode
 from natsort import natsorted
 from PyPDF2 import PdfMerger
 
@@ -46,13 +46,13 @@ frame = ctk.CTk()
 frame.geometry("250x200")
 frame.title("BoardPDF")
 frame.resizable(False, False)
-set_default_color_theme("dark-blue")
+set_appearance_mode("Dark")
 
 label_board_pdf = ctk.CTkLabel(master=frame, text="BoardPDF", width=150, height=30, text_color="white")
 label_board_pdf.configure(font=("arial", 18))
 label_board_pdf.place(x=50, y=5)
 
-button_select = ctk.CTkButton(master=frame, text= "Diretório origem", width=150, height=30, command=select, fg_color="dark grey", text_color="black")
+button_select = ctk.CTkButton(master=frame, text= "Diretório origem", width=150, height=30, command=select, fg_color="dark grey", text_color="black", hover_color="gray")
 button_select.configure(font=("arial", 14))
 button_select.place(x=50, y=50)
 
@@ -60,7 +60,7 @@ entry_select = ctk.CTkEntry(master=frame, width=200, height=30)
 entry_select.configure(font=("arial", 14))
 entry_select.place(x=25, y=90)
 
-button_execute = ctk.CTkButton(master=frame, text= "Executar", width=150, height=30, command=execute, fg_color="dark grey", text_color="black")
+button_execute = ctk.CTkButton(master=frame, text= "Executar", width=150, height=30, command=execute, fg_color="dark grey", text_color="black", hover_color="gray")
 button_execute.configure(font=("arial", 14))
 button_execute.place(x=50, y=160)
 
